7c0ba114a9a7b8f60e8b1057c3d75c1e0d62d4b7
Raif Coonjah <raifms1200@gmail.com>
2020-08-25 23:13:53 +0400

    v6.2_r3
    - Updated and added hiya, zeropage and linkedin in Samsungbloatware-lite.
    - Merged all changes made from v6.1.2.1
    - Removed all commands in Xtweaks Auto.
    
    Signed-off-by: Raif Coonjah <raifms1200@gmail.com>

--- Pre-release/Xtweaks-auto.sh
+++ Pre-release/Xtweaks-auto.sh
@@ -6,7 +6,7 @@ echo "                _  __ __                          __            ___     __
                    /_/|_|\__/  |__/|__/ \___/ \__,_//_/|_|/____/  /_/  |_|/_// .___//_/ /_/ \__,_/  
                                                                             /_/                     "
 echo "Xtweaks Auto Alpha: V0.1 " 
-echo "Based on: Samsungbloat.txt list v6.1.2" 
+echo "Based on: Samsungbloat.txt list v6.1.2.1" 
 echo "=================== Xtweaks Auto automates the process of debloating your device! ==================="                                                                         
 echo "=================== Before we get start, don't forget to wash your hands! ==================="
 echo "======= Thanks for downloading, if you want to buy me a croissant use this link: paypal.me/RaifS ========="
@@ -15,26 +15,11 @@ I'm not responsible for any damage caused."
 
 read -p "Xtweaks: Press ENTER to continue or press CTRL + C on your keyboard to quit";echo
 
-echo "--- Useless/General bloat --"
-adb shell pm uninstall -k --user 0 com.dsi.ant.sample.acquirechannels
-adb shell pm uninstall -k --user 0 com.dsi.ant.service.socket
-adb shell pm uninstall -k --user 0 com.dsi.ant.server
-adb shell pm uninstall -k --user 0 com.dsi.ant.plugins.antplus
-adb shell pm uninstall -k --user 0 com.android.egg
-adb shell pm uninstall -k --user 0 com.sec.android.easyonehand
-adb shell pm uninstall -k --user 0 com.sec.android.widgetapp.samsungapps
-adb shell pm uninstall -k --user 0 com.samsung.android.mateagent
-adb shell pm uninstall -k --user 0 com.sec.android.easyMover.Agent
-adb shell pm uninstall -k --user 0 com.samsung.android.app.watchmanagerstub
-adb shell pm uninstall -k --user 0 com.sec.android.daemonapp
-adb shell pm uninstall -k --user 0 com.samsung.svoice.sync
-adb shell pm uninstall -k --user 0 com.samsung.android.svoice
-adb shell pm uninstall -k --user 0 com.samsung.android.svoiceime
-adb shell pm uninstall -k --user 0 de.axelspringer.yana.zeropage
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.ru_RU
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.de_DE
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.fr_FR
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.en_GB
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.en_US
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.es_ES
-adb shell pm uninstall -k --user 0 com.sec.svoice.lang.it_IT
+echo "--- Useless/General bloat --
+
+
+
+
+
+
+Unmerged SamsungBloat.txt
